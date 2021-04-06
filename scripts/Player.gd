a418f87991893c63af8cdf937561545c08af4d6f
hlin91 <hlin91@outlook.com>
2021-04-05 22:57:20 -0700

    Fleshed out digging mechanic

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -9,7 +9,7 @@ var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func get_input():
 	velocity = Vector2()
@@ -26,12 +26,18 @@ func get_input():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	get_input()
-	var collision = move_and_collide(velocity)
+	move_and_slide(velocity)
 	# Get specific tile player has collided with
-	if collision and collision.collider is TileMap:
-		var tile_pos = collision.collider.world_to_map(position)
-		tile_pos -= collision.normal
-		var tile_id = collision.collider.get_cellv(tile_pos)
-		# For testing
-		print(tile_id)
-	
+#	if collision and collision.collider is TileMap:
+#		var tile_pos = collision.collider.world_to_map(position)
+#		tile_pos -= collision.normal
+#		var tile_id = collision.collider.get_cellv(tile_pos)
+#		collision.collider.set_cellv(tile_pos, -1) # Delete the tile
+#		# For testing
+#		print(tile_id)
+	for i in range(get_slide_count()):
+		var collision = get_slide_collision(i)
+		if collision.collider is TileMap:
+			var tile_pos = collision.collider.world_to_map(collision.position - collision.normal)
+			collision.collider.set_cellv(tile_pos, -1)
+			print(tile_pos)
