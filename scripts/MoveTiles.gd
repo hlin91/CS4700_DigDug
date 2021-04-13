6c35abe6266b59fcb646b1637925206b4cbfd3ce
hlin91 <hlin91@outlook.com>
2021-04-13 00:00:02 -0700

    Fixed bug in is_cell_dug

--- scripts/MoveTiles.gd
+++ scripts/MoveTiles.gd
@@ -17,7 +17,7 @@ func add_moved_to_cell(cell):
 #		print("Moved to cell " + str(cell))
 	
 func is_cell_dug(cell):
-	if cell.y > 0 and cell.y <= 8: # Topmost level is always dug out
+	if cell.y == 0: # Topmost level is always dug out
 		return true
 	return cell in dugged_cells
 	
