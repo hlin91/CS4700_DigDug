11cc87ee4af42dfa942ed43696b487a7fa10e21a
NTCalkins <nicholastcalkins@gmail.com>
2021-04-21 23:31:34 -0700

    beginning connection to game manager

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -9,11 +9,11 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in get_tree().get_nodes_in_group("baddies"):
-			connect("baddie_died",i,"_update_score")
+			connect("baddie_died",i,"_on_RedBaddie_baddie_died", [i.base_score, i.current_cell])
 
-func update_score(base_score, current_cell):
-	print(base_score)
-	print(current_cell)
+func _on_RedBaddie_baddie_died(base_score, current_cell):
+	print("GM SAYS BADDIE DIED")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
