f44afe62e8f5d62be000be997d188f1a824304f7
NTCalkins <nicholastcalkins@gmail.com>
2021-04-28 23:01:09 -0700

    added global variable for score

--- scripts/Score.gd
+++ scripts/Score.gd
@@ -8,13 +8,16 @@ var int_score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	text = "Score: 0"
-	print(self)
+	int_score = global.score
+	text = "Score: " + str(int_score)
 	self.rect_position = Vector2(768,64)
-	self.rect_size = Vector2(60,60)
+	self.rect_size = Vector2(120,60)
 
 func update_score(additional_score):
 	int_score += additional_score
+	if global.high_score < int_score:
+		global.high_score = int_score
+	global.score = int_score
 	text = "Score: " + str(int_score)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
