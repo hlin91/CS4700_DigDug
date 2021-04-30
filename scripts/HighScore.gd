b605576a97b0e391fb491dfff3a738ccaf96bcf8
NTCalkins <nicholastcalkins@gmail.com>
2021-04-29 23:54:57 -0700

    added to todolist

--- scripts/HighScore.gd
+++ scripts/HighScore.gd
@@ -4,12 +4,10 @@ extends RichTextLabel
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var int_score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	int_score = global.score
-	text = "High Score: " + str(int_score)
+	text = "High Score: " + str(global.high_score) 
 	self.rect_position = Vector2(768,64)
 	self.rect_size = Vector2(120,60)
 
--- scripts/Score.gd
+++ scripts/Score.gd
@@ -4,7 +4,6 @@ extends RichTextLabel
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var int_score = 0
 
 export var high_score_path = "../HighScore"
 var high_score
@@ -12,18 +11,16 @@ var high_score
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	high_score = get_node(high_score_path)
-	int_score = global.score
-	text = "Score: " + str(int_score)
+	text = "Score: " + str(global.score)
 	self.rect_position = Vector2(768,128)
 	self.rect_size = Vector2(120,60)
 
 func update_score(additional_score):
-	int_score += additional_score
-	if global.high_score < int_score:
-		global.high_score = int_score
+	global.score += additional_score
+	if global.high_score < global.score:
+		global.high_score = global.score
 		high_score.update_score()
-	global.score = int_score
-	text = "Score: " + str(int_score)
+	text = "Score: " + str(global.score)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
