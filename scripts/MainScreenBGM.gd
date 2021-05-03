485ee9d0772945d3271709b9645062b407fb3620
NTCalkins <nicholastcalkins@gmail.com>
2021-05-02 22:55:27 -0700

    added main screen bgm

---
+++ scripts/MainScreenBGM.gd
@@ -0,0 +1,18 @@
+extends AudioStreamPlayer
+
+var bgm = preload("res://assets/sounds/Chiptronical.ogg")
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	stream = bgm
+	volume_db = -12
+	play() 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
