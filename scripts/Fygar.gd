8fe02b692996413fee04f6c8543f3763f38ce848
NTCalkins <nicholastcalkins@gmail.com>
2021-05-11 23:46:07 -0700

    commented fygar

--- scripts/Fygar.gd
+++ scripts/Fygar.gd
@@ -21,6 +21,7 @@ func _ready():
 	base_score = 800
 
 func breathe_fire():
+	#Create the fire and then set the appropriate layers and masking
 	var f = fire.instance()
 	f.start($FirePosition.global_position, rotation)
 	f.set_collision_layer_bit(0, true)
@@ -28,20 +29,24 @@ func breathe_fire():
 	f.set_collision_mask_bit(0, true)
 	f.set_collision_layer_bit(1, false)
 	f.set_collision_mask_bit(1, false)
+	#Timer to set the fire to last only 1 second
 	var t = Timer.new()
 	t.set_wait_time(1)
 	t.set_one_shot(true)
 	self.add_child(t)
+	#Variable to remind fygar to stop
 	breathing_fire = true
 	t.start()
 	sprite.play_firing()
 	yield(t, "timeout")
+	#Only fire if not being inflated
 	if (inflation == 0):
 		get_parent().add_child(f)
 	else:
 		sprite.set_to_walk()
 		breathing_fire = false
 		return
+	#Wait here after shooting fire
 	t.set_wait_time(1.5)
 	t.start()
 	yield(t, "timeout")
@@ -51,10 +56,11 @@ func breathe_fire():
 	walk_speed = original_walk_speed
 
 func _physics_process(delta):
-	
+	#Don't move if breathing fire
 	if breathing_fire:
 		walk_speed = 0
 	if (!is_ghosting):
+		#Shoot fire after some amount of time
 		fire_value += delta
 		if (fire_value >= fire_threshold):
 			print("fygar breathing fire!")
